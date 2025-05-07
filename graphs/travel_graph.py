de88d961bdac11439427db4e718b1ba236f8113e
Mihir100417 <mihir_kulkarni@live.com>
2025-05-07 15:47:25 +0530

    Update travel_graph.py
    Removed the budget agent since it is not working well

--- graphs/travel_graph.py
+++ graphs/travel_graph.py
@@ -7,7 +7,7 @@ from langgraph.graph import StateGraph, END
 from agents.flight_agent import flight_agent
 from agents.hotel_agent import hotel_agent
 from agents.itinerary_agent import itinerary_agent
-from agents.budget_agent import budget_agent
+# from agents.budget_agent import budget_agent
 from agents.save_agent import save_agent
 
 def build_travel_graph():
@@ -21,7 +21,7 @@ def build_travel_graph():
         flights: Optional[List[Dict]] = []
         hotels: Optional[List[Dict]] = []
         itinerary: Optional[List[Dict]] = []
-        budget_estimate: Optional[float] = None
+        # budget_estimate: Optional[float] = None
     
     graph = StateGraph(TravelState)
 
@@ -29,7 +29,7 @@ def build_travel_graph():
     graph.add_node("find_flights", flight_agent)
     graph.add_node("find_hotels", hotel_agent)
     graph.add_node("create_itinerary", itinerary_agent)
-    graph.add_node("calculate_budget", budget_agent)
+    # graph.add_node("calculate_budget", budget_agent)
     graph.add_node("save_trip", save_agent)
 
     # Edges
@@ -37,8 +37,9 @@ def build_travel_graph():
 
     graph.add_edge("find_flights", "find_hotels")
     graph.add_edge("find_hotels", "create_itinerary")
-    graph.add_edge("create_itinerary", "calculate_budget")
-    graph.add_edge("calculate_budget", "save_trip")
+    # graph.add_edge("create_itinerary", "calculate_budget")
+    # graph.add_edge("calculate_budget", "save_trip")
+    graph.add_edge("create_itinerary","save_trip")
     graph.add_edge("save_trip", END)
 
     graph.set_finish_point("save_trip")
