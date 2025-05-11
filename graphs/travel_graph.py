00222b0dc6552f28bde418aa76d0bd95753c9ce3
Mihir100417 <mihir_kulkarni@live.com>
2025-05-11 11:32:54 +0530

    Update travel_graph.py
    Commenting out Save Itinerary option and agent

--- graphs/travel_graph.py
+++ graphs/travel_graph.py
@@ -8,7 +8,7 @@ from agents.flight_agent import flight_agent
 from agents.hotel_agent import hotel_agent
 from agents.itinerary_agent import itinerary_agent
 # from agents.budget_agent import budget_agent
-from agents.save_agent import save_agent
+# from agents.save_agent import save_agent
 
 def build_travel_graph():
 
@@ -30,7 +30,7 @@ def build_travel_graph():
     graph.add_node("find_hotels", hotel_agent)
     graph.add_node("create_itinerary", itinerary_agent)
     # graph.add_node("calculate_budget", budget_agent)
-    graph.add_node("save_trip", save_agent)
+    # graph.add_node("save_trip", save_agent)
 
     # Edges
     graph.set_entry_point("find_flights")
@@ -39,9 +39,11 @@ def build_travel_graph():
     graph.add_edge("find_hotels", "create_itinerary")
     # graph.add_edge("create_itinerary", "calculate_budget")
     # graph.add_edge("calculate_budget", "save_trip")
-    graph.add_edge("create_itinerary","save_trip")
-    graph.add_edge("save_trip", END)
+    # graph.add_edge("create_itinerary","save_trip")
+    # graph.add_edge("save_trip", END)
+    graph.add_edge("create_itinerary", END)
+    # graph.set_finish_point("save_trip")
 
-    graph.set_finish_point("save_trip")
-
+    graph.set_finish_pount("create_itinerary")
+    
     return graph.compile()
