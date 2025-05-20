ed19294c1f1f0f8b9e75ceecb947689329ad44e3
Mihir100417 <mihir_kulkarni@live.com>
2025-05-20 14:50:30 +0530

    Update llm.py
    Moving it to Huggingface model

--- llm.py
+++ llm.py
@@ -1,23 +1,23 @@
-from langchain_openai import ChatOpenAI
-import streamlit as st
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-# from config import OPENAI_API_KEY
-
-llm = ChatOpenAI(
-    openai_api_key = OPENAI_API_KEY,
-    model = "gpt-3.5-turbo-0125",
-    temperature = 0.3
-)
-
-# import os
-# from langchain.llms import HuggingFaceHub
-# # from config import HUGGINGFACEHUB_API_TOKEN
-# # os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
-
+# from langchain_openai import ChatOpenAI
 # import streamlit as st
-# HUGGINGFACEHUB_API_TOKEN = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+# OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+# # from config import OPENAI_API_KEY
 
-# llm = HuggingFaceHub(
-#     repo_id="HuggingFaceH4/zephyr-7b-beta",
-#     model_kwargs={"temperature": 0.3}
+# llm = ChatOpenAI(
+#     openai_api_key = OPENAI_API_KEY,
+#     model = "gpt-3.5-turbo-0125",
+#     temperature = 0.3
 # )
+
+import os
+from langchain.llms import HuggingFaceHub
+# from config import HUGGINGFACEHUB_API_TOKEN
+# os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
+
+import streamlit as st
+HUGGINGFACEHUB_API_TOKEN = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+
+llm = HuggingFaceHub(
+    repo_id="mistralai/Mistral-7B-v0.1",
+    model_kwargs={"temperature": 0.3}
+)
