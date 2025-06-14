23eca3842cb87995b0331f010a1c4f9c9bcfcaac
Mihir100417 <mihir_kulkarni@live.com>
2025-06-14 23:25:04 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 import streamlit as st
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-# from config import OPENAI_API_KEY
+# OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+from config import OPENAI_API_KEY
 
 llm = ChatOpenAI(
     openai_api_key = OPENAI_API_KEY,
@@ -13,17 +13,10 @@ llm = ChatOpenAI(
 
 # import os
 # from langchain.llms import HuggingFaceHub
-# # from config import HUGGINGFACEHUB_API_TOKEN
-# # os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
-
-# import streamlit as st
-# HUGGINGFACEHUB_API_TOKEN = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+# from config import HUGGINGFACEHUB_API_TOKEN       
+# os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
 
 # llm = HuggingFaceHub(
 #     repo_id="HuggingFaceH4/zephyr-7b-beta",
-#     model_kwargs={
-#         "temperature": 0.3,
-#         "max_new_tokens": 512,  
-#         "top_p": 0.95              
-#     }
+#     model_kwargs={"temperature": 0.3}
 # )
