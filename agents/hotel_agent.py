d0f2a18d6f431ab00d814a4820e402afa812fedf
Mihir100417 <mihir_kulkarni@live.com>
2025-06-14 23:31:28 +0530

    Update hotel_agent.py

--- agents/hotel_agent.py
+++ agents/hotel_agent.py
@@ -1,9 +1,11 @@
 # agents/hotel_agent.py
 
-from config import TAVILY_API_KEY
+# from config import TAVILY_API_KEY
+import streamlit as st
 from tavily import TavilyClient
 
-client = TavilyClient(api_key=TAVILY_API_KEY)
+# client = TavilyClient(api_key=TAVILY_API_KEY)
+TAVILY_API_KEY = st.secrets["TAVILY_API_KEY"]
 
 def hotel_agent(state):
     destination = state.destination
